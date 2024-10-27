7eb820da7a19db43dadbdae6b895ed1f2dd97db9
Durgakola <Durgakola50@gmail.com>
2024-10-27 10:25:14 +0530

    dockerjenkins

--- src/main/java/com/neoteric/springbootjenkinsdemo/App.java
+++ src/main/java/com/neoteric/springbootjenkinsdemo/App.java
@@ -1,6 +1,6 @@
 package com.neoteric.springbootjenkinsdemo;
 public class App {
     public static void main( String[] args ){
-      System.out.println( "Hello World " );
+      System.out.println( "Hello my world" );
     }
 }
